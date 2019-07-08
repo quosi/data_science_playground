3ef639982fd7ae29f148868d940df30344ce812f
Paul Wlodkowski <pawlodkowski@gmail.com>
2019-07-08 18:04:40 +0200

    created README schedule

---
+++ README.md
@@ -0,0 +1,31 @@
+# Hypepperameters Schedule
+
+## Week 01: Your Data Science Tools (Jul 8)
+
+| Day | Morning (9:15) | Afternoon (13:30) |
+| - | :--: | :--: |
+| Monday | [Orientation][1] | [Git / Github][2] |
+| Tuesday | [Pandas: Introduction][3] | [Bash][4] |
+| Wednesday | [Plotting in pandas][5] | [Long vs Wide Tables][6] |
+| Thursday | [Data Wrangling in pandas][7] | [recap: Python functions][8] |
+| Friday | [Simple Linear Regression][9] | [recap: Data Structures][10] |
+
+[1]: http://krspiced.pythonanywhere.com
+
+[2]: http://krspiced.pythonanywhere.com
+
+[3]: http://krspiced.pythonanywhere.com
+
+[4]: http://krspiced.pythonanywhere.com
+
+[5]: http://krspiced.pythonanywhere.com
+
+[6]: http://krspiced.pythonanywhere.com
+
+[7]: http://krspiced.pythonanywhere.com
+
+[8]: http://krspiced.pythonanywhere.com
+
+[9]: http://krspiced.pythonanywhere.com
+
+[10]: http://krspiced.pythonanywhere.com
--- make_readme.py
+++ make_readme.py
@@ -3,13 +3,15 @@ Dynamically generate a README file with the schedule
 (in tabular format, with hyperlinks), based on the cohort and week selected.
 """
 
+import argparse
+
 from scheduler import main
 
 COHORT = "sigmoid_saffron"
 WEEK = "1"
 OUTPUT_FILE = "TEST.md"
 
-def make_readme(cohort, week):
+def make_readme(cohort, week, output_file):
     """Using data extracted from the Google Sheets API (by calling the main
        scheduler function), generate a README file (line-by-line) that
        writes the schedule into tabular format with the appropriate data
@@ -24,7 +26,7 @@ def make_readme(cohort, week):
     am = j['am_lectures']
     pm = j['pm_lectures']
 
-    with open(OUTPUT_FILE, 'w') as readme:
+    with open(output_file, 'w') as readme:
         readme.write('# {} Schedule\n\n'.format(cohort_title))
         readme.write('## {}\n\n'.format(sub_title))
         readme.write('| Day | Morning (9:15) | Afternoon (13:30) |')
@@ -41,8 +43,24 @@ def make_readme(cohort, week):
         for i in range(1, 2*len(wkdy) + 1):
             readme.write(f'\n\n[{i}]: {URL}')
 
-        print(f'Successfully generated {OUTPUT_FILE}')
+        print(f'Successfully generated {output_file}')
 
 
 if __name__ == '__main__':
-    make_readme(COHORT, WEEK)
+
+    parser = argparse.ArgumentParser(description='Dynamically generate a README\
+    file with the schedule (in tabular format, with hyperlinks),\
+    based on the cohort and week selected.')
+
+    parser.add_argument('-c', '--cohort', type=str, required=True,
+                   help='Enter cohort name (e.g. sigmoid_saffron)')
+
+    parser.add_argument('-w', '--week', type=str, required=True,
+                   help='Enter week number.')
+
+    parser.add_argument('-o', '--output_file', type=str, required=True,
+                   help='Enter name of markdown file to output (e.g. README.md)')
+
+    args = parser.parse_args()
+
+    make_readme(args.cohort, args.week, args.output_file)
